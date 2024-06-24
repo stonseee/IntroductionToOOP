47c94af6836f9d83386f454185ade8cd94eece9c
stonseee <stonseee@STONSEEE>
2024-06-24 16:38:58 +0300

    homework done!!

--- String/main.cpp
+++ String/main.cpp
@@ -10,92 +10,126 @@ class String
 {
 private:
 
-	int SIZE;
+	int size;
 	char* str;
 
 public:
 
-	int get_size()
+	int get_size()const
 	{
-		return this->SIZE;
+		return this->size;
 	}
-	char* get_string()
+	const char* get_str()const
 	{
 		return this->str;
 	}
-
-	void set_size(int SIZE)
+	char* get_str()
 	{
-		this->SIZE = SIZE;
+		return this->str;
 	}
-	void set_string(const char* str)
-	{		
-		this->SIZE = strnlen_s(str, INT_MAX) + 1;			
-		delete[] this->str;
-	    this->str = new char[this->SIZE];
-		strcpy_s(this->str, SIZE, str);	
-	}
-
+	
 	//constructors
-	String() : String(80)
+	explicit String(int size = 80)
 	{
+		this->size = size;
+		this->str = new char[size] {};
 		cout << "defaultConstructor\t" << this << endl;
 	}
-	String(int SIZE)
+	String(const char str[])
 	{
-		this->SIZE = SIZE + 1;
-		this->str = new char[this->SIZE];
-		cout << "sizeArgumentConstructor\t" << this << endl;
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = str[i];
+		cout << "constructor\t\t" << this << endl;
 	}
-	String(const char* str) : String(strnlen_s(str, INT_MAX))
-	{		
-		strcpy_s(this->str, this->SIZE, str);
-		cout << "charArgumentConstructor\t" << this << endl;
-	}	
-	~String()
+	String(const String& other)
 	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++) this->str[i] = other.str[i];
+		cout << "copyConstructor" << this << endl;
+
+	}
+	~String()
+	{		
 		delete[] str;
-		cout << "destructor\t" << this << endl;
-	}	
+		cout << "destructor\t\t" << this << endl;
+	}
+
+	//operators
+	String& operator=(const String& other)
+	{
+		if (this == &other) return *this;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++) this->str[i] = other.str[i];		
+		cout << "copyAssignment\t\t" << this << endl;
+		return *this;
+	}
+
+	char operator[](const int idx) const
+	{
+		return this->str[idx];
+	}
+
+	char& operator[](const int idx)
+	{
+		return this->str[idx];
+	}
+
+	//methods
+	void print()const
+	{
+		cout << "obj:\t\t\t" << this << endl;
+		cout << "size:\t\t\t" << size << endl;
+		cout << "addr:\t\t\t" << &str << endl;
+		cout << "str:\t\t\t" << str << endl;
+		cout << delimiter << endl;
+	}
 };
 
-//operators
-String operator+(String& left, String& right)
+String operator+(const String& left, const String& right)
 {
-	String result(left.get_size() + right.get_size() - 1);
-	strcpy_s(result.get_string(), left.get_size(), left.get_string());
-	strcpy_s(result.get_string() + left.get_size() - 1, right.get_size(), right.get_string());
-	return result;
+	String buffer(left.get_size() + right.get_size() - 1);
+
+	for (int i = 0; i < left.get_size(); i++)
+	{
+		buffer[i] = left[i];
+	}
+	for (int i = 0; i < right.get_size(); i++)
+		buffer[i + left.get_size() - 1] = right[i];
+	return buffer;
 }
 
-std::ostream& operator<<(ostream& output, String& obj)
+std::ostream& operator<<(std::ostream& os, const String& obj)
 {
-	output << "(" << obj.get_string() << ")";
-	return output;
-}
-
-std::istream& operator>>(istream& input, String& obj)
-{
-	input >> obj.get_string();
-	return input;
+	return os << obj.get_str();
 }
 
 void main()
 {
     setlocale(LC_ALL, "");
 
-	String str1;		
-	cout << str1 << endl;
+	String str1;
+	str1.print();
 
+	//String str2 = 8;
 	String str2(8);
-	cout << str2 << endl;
+	str2.print();
 
-	String str3 = "Hello";		
-	cout << str3 << endl;
-	
+	String str3 = "Hello";
+	str3 = str3;
+	str3.print();
+
 	String str4 = "World";
+	str4.print();
+
+	cout << str3 << endl;
 	cout << str4 << endl;
 
-	String str5 = str3 + str4;
+	//String str5 = str3 + str4;
+	String str5;
+	str5 = str3 + str4;
+	str5.print();
 	cout << str5 << endl;
 }
